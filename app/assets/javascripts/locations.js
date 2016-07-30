0e04b6b0902b5baf63ed4cafda6ed4420c8df213
emily.c.ritter <emily.c.ritter@gmail.com>
2016-07-30 00:51:44 -0500

    Styling for map

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -40,9 +40,11 @@ $(document).ready(function() {
     var lat_lng = $(map_div).html();
     if (lat_lng.length > 0) {
       var api_key = 'pk.eyJ1IjoiZW1pbHljcml0dGVyIiwiYSI6ImNpbWdmZXhmYzAyMDV1NGx2bHM0MTNzNGYifQ.7KYzLItfXBfg5Zs-757BGw';
-      var mapbox_anchor = '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>';
-      var map_img = '<div><img width="400" src="https://api.mapbox.com/v4/mapbox.high-contrast/pin-m-circle+285A98(' + lat_lng + ')/' + lat_lng + ',13/400x400.png?access_token=' + api_key + '">' + mapbox_anchor + '</div>';
+      var mapbox_anchor = '<div class="map-box-links">©<a href="https://www.mapbox.com/map-feedback/"> Mapbox</a> ©<a href="http://www.openstreetmap.org/copyright"> OpenStreetMap contributors</a></div>';
+      var map_img = '<div><img width="300" src="https://api.mapbox.com/v4/mapbox.high-contrast/pin-m-circle+285A98(' + lat_lng + ')/' + lat_lng + ',13/400x400.png?access_token=' + api_key + '">' + mapbox_anchor + '</div>';
       $(map_div).html(map_img);
+      var width = $(map_div).children('img').width();
+      $(map_div).children('img').css('height', width);
     }
     $(closest_map_container).show();
   });
